9f3f1e8324e28beb1e6e4738045b61bf8235753a
Juni Yadi <juni.yadi@pintar.co>
2023-10-10 15:59:42 +0700

    Update Generator

--- generate.sh
+++ generate.sh
@@ -18,23 +18,55 @@ ID_BLOG_DESTINATION="$i18n/id/$BLOG_PATH_DESTINATION"
 find contents/blog -type f -name '*.en.md' > list-en.txt
 find contents/blog -type f -name '*.id.md' > list-id.txt
 
-echo "Copy Files Blog English to $EN_BLOG_DESTINATION"
+echo "Check Multiple Folder Blog Translations"
 
-while read line; do
-    echo "Copy $line to $EN_BLOG_DESTINATION"
+if [ -d "contents/blog/en/" ]; then
+    echo "Folder content/blog/en/ Exist, Copy Files to $EN_BLOG_DESTINATION"
+    cp -r contents/blog/en/*.md $EN_BLOG_DESTINATION
+    cp -r contents/blog/en/*.md blog
+else
+    echo "Folder Blog English Not Exist"
+fi
 
-    FILENAME=$(basename $line)
-    cp $line "$EN_BLOG_DESTINATION/${FILENAME%.en.md}.md"
-    cp $line "blog/${FILENAME%.en.md}.md"
+if [ -d "contents/blog/id/" ]; then
+    echo "Folder content/blog/id/ Exist, Copy Files to $EN_BLOG_DESTINATION"
+    cp -r contents/blog/id/*.md $EN_BLOG_DESTINATION
+    cp -r contents/blog/id/*.md blog
+else
+    echo "Folder Blog Indonesia Not Exist"
+fi
 
-done < list-en.txt
 
-echo "Copy Files Blog Indonesia to $ID_BLOG_DESTINATION"
+# Count line in list-en.txt
+COUNT_EN=$(awk 'END {print NR}' list-en.txt)
 
-while read line; do
-    echo "Copy $line to $ID_BLOG_DESTINATION"
 
-    FILENAME=$(basename $line)
-    cp $line "$ID_BLOG_DESTINATION/${FILENAME%.id.md}.md"
+if [[ $COUNT_EN -gt 0 ]]; then
 
-done < list-id.txt
+    echo "Copy Files Blog English From contents/blog to $EN_BLOG_DESTINATION"
+
+    while read line; do
+        echo "Copy $line to $EN_BLOG_DESTINATION"
+
+        FILENAME=$(basename $line)
+        cp $line "$EN_BLOG_DESTINATION/${FILENAME%.en.md}.md"
+        cp $line "blog/${FILENAME%.en.md}.md"
+
+    done < list-en.txt
+fi
+
+# Count line in list-id.txt
+COUNT_ID=$(awk 'END {print NR}' list-id.txt)
+
+if [ $COUNT_ID -gt 0 ]; then
+
+    echo "Copy Files Blog Indonesia From contents/blog to $ID_BLOG_DESTINATION"
+
+    while read line; do
+        echo "Copy $line to $ID_BLOG_DESTINATION"
+
+        FILENAME=$(basename $line)
+        cp $line "$ID_BLOG_DESTINATION/${FILENAME%.id.md}.md"
+
+    done < list-id.txt
+fi
