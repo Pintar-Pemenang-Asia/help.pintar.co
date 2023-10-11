210856f27bb6993a40d240cebcad0466aad99cc5
Juni Yadi <juni.yadi@pintar.co>
2023-10-11 14:23:38 +0700

    Update Docusaurus Config - Author and Time

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,6 +49,8 @@ const config = {
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/Pintar-Pemenang-Asia/help.pintar.co/blob/main',
+          showLastUpdateAuthor: true,
+          showLastUpdateTime: true,
         },
         blog: {
           showReadingTime: true,
